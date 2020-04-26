044d205bc55a61b779cbf17a77ddc61d2c31eec8
John Alvarez <jcalvare@johns-mbp.home>
2020-04-26 15:06:52 -0400

    dropdown + categories

--- js/dataAgent.js
+++ js/dataAgent.js
@@ -6,9 +6,14 @@ class FGF {
 };
 var xmlhttp = new XMLHttpRequest();
 var myObj;
+var list = [];
+var c = document.getElementById('myDropdown');
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
     myObj = JSON.parse(this.responseText);
+    list = Object.keys(myObj[0]);
+
+
 
  
 
@@ -17,3 +22,5 @@ console.log(myObj);
 };
 xmlhttp.open("GET", `./covid_data.json`, true);
 xmlhttp.send(); 
+
+
